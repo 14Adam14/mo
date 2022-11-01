3cd3af96c8e2d5e432b62a0d21469b642703481e
ve.meoso <adam.bolotov@gmail.com>
2022-11-01 04:42:00 +0400

    39+

---
+++ Shared/ThermometerScaleView.swift
@@ -0,0 +1,23 @@
+//
+//  ThermometerScaleView.swift
+//  mo (iOS)
+//
+//  Created by Adam on 01.11.2022.
+//
+
+import SwiftUI
+
+struct ThermometerScaleView: View {
+    private var scaleSize: CGFloat = 276
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThermometerScaleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThermometerScaleView()
+    }
+}
